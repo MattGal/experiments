c4169093a41c45f20364bbe4e6b142cab9cfd00e
Matt Galbraith <mattgal@microsoft.com>
2020-01-22 13:24:16 -0800

    three more variants

--- enable-cross-org-publishing-arcade.ps1
+++ enable-cross-org-publishing-arcade.ps1
@@ -20,5 +20,16 @@ Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES4' -Value 'https://dnceng
 Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN5' -Value "$token" -IsMultiJobVariable $true
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES5' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -IsMultiJobVariable $true
 
+# Does seemingly nothing but makes the previous value a secret (and get ***)
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN6' -Value "$token" -Secret
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES6' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -Secret -IsMultiJobVariable $false
+
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN7' -Value "$token" -AsOutput
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES7' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/'  -AsOutput -IsMultiJobVariable $false
+
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN8' -Value "$token" -IsMultiJobVariable $true
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES8' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -IsMultiJobVariable $false
+
+
 
 
