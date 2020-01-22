44c7b8dbcfd49297c00cdcf41a6cce968745ffa7
Matt Galbraith <mattgal@microsoft.com>
2020-01-22 12:41:39 -0800

    That didn't work.  Experiment 2:  Flip every switch independently

--- enable-cross-org-publishing-arcade.ps1
+++ enable-cross-org-publishing-arcade.ps1
@@ -6,5 +6,17 @@ param(
 
 $ci = $true
 
-Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN2' -Value $token
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN2' -Value $token 
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES2' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/'
+
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN3' -Value $token -Secret
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES3' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -Secret
+
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN4' -Value $token -AsOutput
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES4' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/'  -AsOutput
+
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN5' -Value $token -IsMultiJobVariable $true
+Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES5' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -IsMultiJobVariable $true
+
+
+
