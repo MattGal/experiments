b172f4edbd5b802d75085f0a41d4c4b0efbdb637
Matt Galbraith <mattgal@microsoft.com>
2020-01-22 12:48:32 -0800

    Trying some more quotes

--- enable-cross-org-publishing-arcade.ps1
+++ enable-cross-org-publishing-arcade.ps1
@@ -6,16 +6,18 @@ param(
 
 $ci = $true
 
+# Does (seemingly) nothing
 Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN2' -Value $token 
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES2' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/'
 
-Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN3' -Value $token -Secret
+# Does seemingly nothing but makes the previous value a secret (and get ***)
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN3' -Value "$token" -Secret
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES3' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -Secret
 
-Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN4' -Value $token -AsOutput
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN4' -Value "$token" -AsOutput
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES4' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/'  -AsOutput
 
-Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN5' -Value $token -IsMultiJobVariable $true
+Write-PipelineSetVariable -Name 'VSS_NUGET_ACCESSTOKEN5' -Value "$token" -IsMultiJobVariable $true
 Write-PipelineSetVariable -Name 'VSS_NUGET_URI_PREFIXES5' -Value 'https://dnceng.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/dnceng/;https://devdiv.pkgs.visualstudio.com/;https://pkgs.dev.azure.com/devdiv/' -IsMultiJobVariable $true
 
 
