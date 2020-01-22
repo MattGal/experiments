0d9e6b427c6442a8ecc655b1a16d7b4b25e40c98
Matt Galbraith <mattgal@microsoft.com>
2020-01-22 12:30:48 -0800

    hard code msbuild path

--- doStuff.cmd
+++ doStuff.cmd
@@ -1,1 +1,1 @@
-msbuild /flp:v=diag /clp:v=diag experimental.proj
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /flp:v=diag /clp:v=diag experimental.proj
